346a791e46030437df32a0f493fd03048bfce3e4
Tatut Listya Maharani <tatutlistya2405@gmail.com>
2023-08-14 12:23:17 +0700

    update sekuy

--- main.js
+++ main.js
@@ -111,6 +111,88 @@ tabs.forEach((tab, index)=>{
         all_artikel_text[index].classList.add('active');
     });
 })
+// Javascript for Sepeda Motor //
+const navspdmotor = document.querySelector("#sepedamotor-disp");
+const dispmotor = document.querySelector(".Layanan-motor");
+const terpopuler = document.querySelector(".terpopuler");
+const terkait = document.querySelector(".terkait");
+const populermotor = document.querySelector(".Populer-motor");
+const terkaitmotor = document.querySelector(".Terkait-motor"); 
+
+navspdmotor.onclick = (()=>{
+    navspdmotor.classList.add('active');
+    dispmotor.classList.add('active');
+    navmobil.classList.remove('active');
+    dispmobil.classList.remove('active');
+});
+
+terkait.onclick = (()=>{
+    terkait.classList.add('active');
+    terpopuler.classList.remove('active');
+    terkaitmotor.classList.add('active');
+    populermotor.classList.remove('active');
+});
+terpopuler.onclick = (()=>{
+    terkait.classList.remove('active');
+    terpopuler.classList.add('active');
+    terkaitmotor.classList.remove('active');
+    populermotor.classList.add('active');
+});
+
+//Javascript for Mobil//
+const navsmobil = document.querySelector("#mobil-disp");
+const dispmobil = document.querySelector(".Layanan-mobil");
+const terpopulerm = document.querySelector(".Layanan-mobil .terpopuler");
+const terkaitm = document.querySelector(".Layanan-mobil .terkait");
+const populermobil = document.querySelector(".Populer-mobil");
+const terkaitmobil = document.querySelector(".Terkait-mobil"); 
+const menunavmobil = document.querySelector (".navmenumobil");
+const navmobil = document.querySelector(".nav-m");
+const navdmobil = document.querySelector(".nav-dm");
+const dispjm = document.querySelector(".dispjm");
+const dispjdm = document.querySelector(".dispjdm");
+const backnavm = document.querySelector(".dispjm .backnav");
+const backnavdm = document.querySelector(".dispjdm .backnav");
+
+navsmobil.onclick = (()=>{
+    navsmobil.classList.add('active');
+    dispmobil.classList.add('active');
+    navspdmotor.classList.remove('active');
+    dispmotor.classList.remove('active');
+});
+
+terkaitm.onclick = (()=>{
+    terkaitm.classList.add('active');
+    terpopulerm.classList.remove('active');
+    terkaitmobil.classList.add('active');
+    populermobil.classList.remove('active');
+});
+terpopulerm.onclick = (()=>{
+    terkaitm.classList.remove('active');
+    terpopulerm.classList.add('active');
+    terkaitmobil.classList.remove('active');
+    populermobil.classList.add('active');
+});
+navmobil.onclick = (()=>{
+    menunavmobil.classList.remove('active');
+    dispjm.classList.add('active');
+});
+navdmobil.onclick = (()=>{
+    menunavmobil.classList.remove('active');
+    dispjdm.classList.add('active');
+});
+backnavm.onclick = (()=>{
+    menunavmobil.classList.add('active');
+    dispjm.classList.remove('active');
+});
+backnavdm.onclick = (()=>{
+    menunavmobil.classList.add('active');
+    dispjdm.classList.remove('active');
+})
+
+
+
+
 // Javascript for Location //
 // const inputkabkota = document.querySelector("#inkabkota-input");
 
